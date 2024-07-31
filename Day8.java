184114c0be16cde966b92a9b8ed401d7d7ac0ea4
tayal0987 <122766729+tayal0987@users.noreply.github.com>
2024-07-31 13:36:16 +0530

    Day8 code added

---
+++ Day8.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int Start = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[Start] != nums[i]) {
+                Start++;
+                nums[Start] = nums[i];
+            }
+        }
+        return Start + 1;
+
+    }
+}
