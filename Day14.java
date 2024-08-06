74149522cf480bc064e2d045aa3842b95f4c0c96
tayal0987 <122766729+tayal0987@users.noreply.github.com>
2024-08-06 11:04:33 +0530

    Update Day14.java

--- Day14.java
+++ Day14.java
@@ -1,3 +1,4 @@
+// Valid Anagram 
 class Solution {
     public boolean isAnagram(String s, String t) {
         // If lengths are not equal, they can't be anagrams
@@ -12,5 +13,18 @@ class Solution {
 
         // compare the arrays
         return Arrays.equals(sArr, tArr);
+    }
+}
+
+// Fibonacci Number
+
+class Solution {
+    public int fib(int n) {
+        if(n==1 || n==0){
+            return n;
+        }
+    
+        return fib(n-1) + fib(n-2);
+        
     }
 }
