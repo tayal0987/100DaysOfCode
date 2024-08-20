581b9ecd1f3816305bb6a6b14a02d7154d477a09
tayal0987 <122766729+tayal0987@users.noreply.github.com>
2024-08-20 11:34:30 +0530

    Add files via upload

---
+++ Day28.java
@@ -0,0 +1,21 @@
+/*** Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }*/
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode Slow = head;
+        ListNode Fast = head;
+
+        while(Fast != null && Fast.next != null){
+            Slow = Slow.next;
+            Fast = Fast.next.next;
+        }
+        return Slow;
+        
+    }
+}
